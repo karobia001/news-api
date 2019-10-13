6f48e1c43d758884bfc2fd7ad1415fda43eb6c5d
karobia001 <karobiamaina81@gmail.com>
2019-10-13 16:28:29 +0300

    adds for loops into our index.html for our upcoming news

--- app/requests.py
+++ app/requests.py
@@ -26,6 +26,30 @@ def get_news(category):
         if get_news_response['results']:
             news_results_list = get_news_response['results']
             news_results = process_results(news_results_list)
+
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        title = news_item.get('original_title')
+        overview = news_item.get('overview')
+        poster = news_item.get('poster_path')
+        vote_average = news_item.get('vote_average')
+        vote_count = news_item.get('vote_count')
+
+        if poster:
+            news_object = News(id,title,overview,poster,vote_average,vote_count)
+            news_results.append(news_object)
             
             
             return news_results
--- app/views.py
+++ app/views.py
@@ -21,4 +21,18 @@ def news(news_id):
     '''
     return render_template('news.html',title = title)
 
+from .request import get_news
 
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+ # Getting popular movie
+    popular_news = get_news('popular')
+    upcoming_news = get_news('upcoming')
+    now_showing_news = get_news('now_playing')
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news)
