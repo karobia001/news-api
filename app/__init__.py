895769a036448be0a44a23f9fc4f29aaec3e9517
karobia001 <karobiamaina81@gmail.com>
2019-10-14 09:09:54 +0300

    fix the app structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from .config import DevConfig
 
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+app = Flask(__name__)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -8,31 +8,4 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
-    title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title)
-
-
-@app.route('/news/<news_id>')
-def news(news_id):
-
-    '''
-    View news page function that returns the news details page and its data
-    '''
-    return render_template('news.html',title = title)
-
-from .request import get_news
-
-@app.route('/')
-def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
- # Getting popular movie
-    popular_news = get_news('popular')
-    upcoming_news = get_news('upcoming')
-    now_showing_news = get_news('now_playing')
-    title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news)
+    return render_template('index.html')
